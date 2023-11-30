7f92081767ddc0fe6de76498a5de55f4ec1297d5
Diwash Lamichhane <imdiwash.mrprogrammer@gmail.com>
2023-11-30 15:10:50 +0545

    feat: Added Name popup and greetings

--- lib/layout/appbar.dart
+++ lib/layout/appbar.dart
@@ -9,48 +9,138 @@ class Appbar extends StatefulWidget {
 }
 
 class _AppbarState extends State<Appbar> {
-  String name = 'Diwash Lamichhane';
+  String name = '';
+  String greetings = "Good Morning";
 
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _loadName();
+    __gretingText();
+    _loadName();
+  }
+
+  __gretingText() {
+    var hour = DateTime.now().hour;
+    if (hour < 12) {
+      greetings = "Good Morning";
+    } else if (hour < 17) {
+      greetings = "Good Afternoon";
+    } else {
+      greetings = "Good Evening";
+    }
+
+    setState(() {
+      greetings = greetings;
     });
   }
 
   _loadName() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    name = (prefs.getString('name') ?? 'Diwash Lamichhane');
+    name = (prefs.getString('name') ?? '');
+
     if (name.isEmpty) {
       _askForName();
+    } else {
+      setState(() {
+        name = name;
+      });
     }
   }
 
   _askForName() async {
-    await showDialog<String>(
-      context: context,
-      barrierDismissible: false, // user must tap button!
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Enter your name'),
-          content: TextField(
-            onChanged: (value) {
-              name = value;
-            },
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('OK'),
-              onPressed: () async {
-                SharedPreferences prefs = await SharedPreferences.getInstance();
-                await prefs.setString('name', name);
-                Navigator.of(context).pop();
-              },
+    await Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        fullscreenDialog: true,
+        builder: (BuildContext context) {
+          return Scaffold(
+            body: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Enter your name',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF4A4A4A)),
+                    ),
+                    SizedBox(height: 12),
+                    Container(
+                      // Set the width according to your needs
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Color.fromARGB(255, 217, 217,
+                              217), // Change the border color as desired
+                        ),
+                        borderRadius: BorderRadius.circular(50),
+                        // Adjust the border radius as needed
+                      ),
+                      child: SizedBox(
+                        height: 45,
+                        child: TextField(
+                          onChanged: (value) {
+                            setState(() {
+                              name = value;
+                            });
+                          },
+                          decoration: InputDecoration(
+                            hintText: "eg: John Doe..",
+                            hintStyle: TextStyle(
+                                color: Color.fromARGB(255, 176, 176, 176)),
+                            border: InputBorder
+                                .none, // Remove default input field border
+                            prefixIcon: Icon(
+                              Icons.person_2_rounded,
+                              color: Color.fromARGB(255, 217, 217,
+                                  217), // Change the color of the search icon
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    TextButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.resolveWith<Color?>(
+                          (Set<MaterialState> states) {
+                            return Color(
+                                0xFFFC7A43); // Background color when pressed
+                            // Default background color
+                          },
+                        ),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                                50.0), // Adjust the radius as needed
+                          ),
+                        ),
+                        padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                            EdgeInsets.symmetric(vertical: 2, horizontal: 20)),
+                      ),
+
+                      onPressed: () async {
+                        SharedPreferences prefs =
+                            await SharedPreferences.getInstance();
+                        await prefs.setString('name', name);
+                        Navigator.of(context).pop();
+                      },
+
+                      child: Text(
+                        'Save Name',
+                        style: TextStyle(color: Colors.white, fontSize: 14),
+                      ), // <-- Text
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ],
-        );
-      },
+          );
+        },
+      ),
     );
   }
 
@@ -89,7 +179,7 @@ class _AppbarState extends State<Appbar> {
               direction: Axis.vertical,
               children: [
                 Text(
-                  'Good Morning',
+                  greetings,
                   style: TextStyle(color: Color(0xFFA5A5A5), fontSize: 12),
                 ),
                 Text(
